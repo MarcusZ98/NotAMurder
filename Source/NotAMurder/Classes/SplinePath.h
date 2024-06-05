cc6d1c5785596b085609c7f8d38847dd0bea6bd2
MarcusZ98 <marcus.zibung@gmail.com>
2024-06-06 00:04:53 +0200

    Spline otw

---
+++ Source/NotAMurder/Classes/SplinePath.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NotAMurder/Interfaces/Speedable.h"
+#include "SplinePath.generated.h"
+
+UCLASS()
+class NOTAMURDER_API ASplinePath : public AActor, public ISpeedable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASplinePath();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Speed")
+	float MovementSpeed = 100.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual float GetSpeed_Implementation() override;
+
+	virtual void SetSpeed_Implementation(float NewSpeed) override;
+
+	virtual void ResetSpeed_Implementation() override;
+
+private:
+	UPROPERTY()
+	float OriginalSpeed;
+
+};
---
+++ Source/NotAMurder/Interfaces/Speedable.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Speedable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class USpeedable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class NOTAMURDER_API ISpeedable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Speed")
+	float GetSpeed();
+	virtual float GetSpeed_Implementation() = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Speed")
+	void SetSpeed(float NewSpeed);
+	virtual void SetSpeed_Implementation(float NewSpeed) = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Speed")
+	void ResetSpeed();
+	virtual void ResetSpeed_Implementation() = 0;
+};

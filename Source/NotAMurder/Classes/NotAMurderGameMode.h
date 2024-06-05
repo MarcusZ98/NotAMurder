dd3f8cbcaae43c55af53c6e7a43e482d8df9bccd
MarcusZ98 <marcus.zibung@gmail.com>
2024-06-05 18:15:56 +0200

    Initial commit

---
+++ Source/NotAMurder/Classes/NotAMurderGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NotAMurderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANotAMurderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANotAMurderGameMode();
+};
+
+
+
---
+++ Source/NotAMurder/NotAMurder.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NotAMurder.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NotAMurder, "NotAMurder" );
+ 

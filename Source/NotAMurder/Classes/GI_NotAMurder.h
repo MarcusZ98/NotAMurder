3bb6de66d90f76dbdd5e3b2189a3e0f3fb40f250
MarcusZ98 <marcus.zibung@gmail.com>
2024-08-31 12:31:24 +0200

    Added: SaveGame (All score and rank is saved and displayed in main menu) & Reset button added to settings to reset the save if needed (For testing mainly)

--- Source/NotAMurder/Classes/GI_NotAMurder.h
+++ Source/NotAMurder/Classes/GI_NotAMurder.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "SaveGame_NotAMurder.h"
 #include "Engine/GameInstance.h"
 #include "GI_NotAMurder.generated.h"
 
@@ -16,8 +17,42 @@ class NOTAMURDER_API UGI_NotAMurder : public UGameInstance
 
 public:
 
+	UGI_NotAMurder();
+
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Levels")
+	TArray<FString> Levels; 
+
 	///////////////////// Variables for the settings ///////////////////////
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings")
 	float Sensitivity = 20.0f;
 
+
+
+
+	///////////////////// Handling Save/Load ///////////////////////
+
+public:
+	
+	// Save game data
+	UFUNCTION(BlueprintCallable, Category = "Save/Load")
+	void SaveData(const FString& LevelName, const int32 HighestScore, const int32 HighestRank);
+
+	// Load game data
+	UFUNCTION(BlueprintCallable, Category = "Save/Load")
+	FLevelData LoadData(const FString& LevelName);
+
+	// Reset save game data
+	UFUNCTION(BlueprintCallable, Category = "Save/Load")
+	void ResetData();
+
+
+private:
+
+	UPROPERTY()
+	FString SlotName = TEXT("LevelDataSlot");
+
+	UPROPERTY()
+	int32 UserIndex = 0;
+	
 };
